f311d4f1a151e4fcf71c01e6b84543e66d8a89b8
Panasenko Roman <zzoza1219@gmail.com>
2025-07-20 22:01:12 +0300

    Добавил временный маршрут для запуска миграций

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Миграции выполнены успешно!';
+});
+
 
 Route::get('/', function () {
     return view('welcome');
