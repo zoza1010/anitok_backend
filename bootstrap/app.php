d0af2942f767c3e40ce1a032798242a4d4984b80
Panasenko Roman <zzoza1219@gmail.com>
2025-07-26 09:32:16 +0300

    Disable AutoMigrate middleware to fix 500 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('api', [
-            \App\Http\Middleware\AutoMigrate::class,
+            // \App\Http\Middleware\AutoMigrate::class, // Временно отключено
             \Illuminate\Routing\Middleware\ThrottleRequests::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
